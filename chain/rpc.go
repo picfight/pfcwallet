1d8c165608bbdddf1e4f95b83e37ade128430c4b
J Fixby <github@jfixby.com>
2019-09-06 11:57:48 +0200

    buildFilterBlocksWatchList

--- chain/rpc.go
+++ chain/rpc.go
@@ -13,6 +13,7 @@ import (
 	"github.com/picfight/pfcd/chaincfg/chainhash"
 	"github.com/picfight/pfcd/pfcjson"
 	"github.com/picfight/pfcd/rpcclient"
+	"github.com/picfight/pfcd/txscript"
 	"github.com/picfight/pfcd/wire"
 	"github.com/picfight/pfcutil"
 	"github.com/picfight/pfcutil/gcs"
@@ -193,6 +194,50 @@ func (c *RPCClient) BlockStamp() (*waddrmgr.BlockStamp, error) {
 	case <-c.quit:
 		return nil, errors.New("disconnected")
 	}
+}
+
+// buildFilterBlocksWatchList constructs a watchlist used for matching against a
+// cfilter from a FilterBlocksRequest. The watchlist will be populated with all
+// external addresses, internal addresses, and outpoints contained in the
+// request.
+func buildFilterBlocksWatchList(req *FilterBlocksRequest) ([][]byte, error) {
+	// Construct a watch list containing the script addresses of all
+	// internal and external addresses that were requested, in addition to
+	// the set of outpoints currently being watched.
+	watchListSize := len(req.ExternalAddrs) +
+		len(req.InternalAddrs) +
+		len(req.WatchedOutPoints)
+
+	watchList := make([][]byte, 0, watchListSize)
+
+	for _, addr := range req.ExternalAddrs {
+		p2shAddr, err := txscript.PayToAddrScript(addr)
+		if err != nil {
+			return nil, err
+		}
+
+		watchList = append(watchList, p2shAddr)
+	}
+
+	for _, addr := range req.InternalAddrs {
+		p2shAddr, err := txscript.PayToAddrScript(addr)
+		if err != nil {
+			return nil, err
+		}
+
+		watchList = append(watchList, p2shAddr)
+	}
+
+	for _, addr := range req.WatchedOutPoints {
+		addr, err := txscript.PayToAddrScript(addr)
+		if err != nil {
+			return nil, err
+		}
+
+		watchList = append(watchList, addr)
+	}
+
+	return watchList, nil
 }
 
 // FilterBlocks scans the blocks contained in the FilterBlocksRequest for any
