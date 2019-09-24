a8fab76b4da285b81966f421d4a45e9db0e12350
J Fixby <github@jfixby.com>
2019-09-24 21:05:34 +0200

    set params

--- params.go
+++ params.go
@@ -1,10 +1,22 @@
 // Copyright (c) 2013-2015 The btcsuite developers
-// Copyright (c) 2015 The Decred developers
+// Copyright (c) 2016-2017 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 package main
 
-import "github.com/decred/dcrwallet/netparams"
+import (
+	"github.com/decred/dcrwallet/netparams"
+	"github.com/picfight/pfcd/picfightcoin"
+)
 
-var activeNet = &netparams.MainNetParams
+var activeNet = &PfcNetParams
+
+// PfcNetParams contains parameters specific running dcrwallet and
+// dcrd on the main network (wire.MainNet).
+var PfcNetParams = netparams.Params{
+	Params:            &picfightcoin.PicFightCoinNetParams,
+	JSONRPCClientPort: "9109",
+	JSONRPCServerPort: "9110",
+	GRPCServerPort:    "9111",
+}
