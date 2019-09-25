c45a497ad05f9fff5043e6c40c5f3f9e75fd6aaf
J Fixby <github@jfixby.com>
2019-09-25 22:25:03 +0200

    example_run

---
+++ example_run.bat
@@ -0,0 +1,1 @@
+pfcwallet       --rpclisten=127.0.0.1:20002       --rpcconnect=127.0.0.1:30001       --appdata=wallet       --cafile=wallet\pfcd-rpc.cert       --rpckey=wallet\rpc.key       --rpccert=wallet\rpc.cert       --pfcdusername=node.user       --pfcdpassword=node.pass       --username=wallet.user       --password=wallet.pass
--- run_wallet.bat
+++
@@ -1,1 +0,0 @@
-pfcwallet --rpclisten=127.0.0.1:20002 --rpcconnect=127.0.0.1:30001 --appdata=wallet --cafile=..\nodeA\rpc.cert --rpckey=wallet\rpc.key --rpccert=rpc.cert --pfcdusername=node.user --pfcdpassword=node.pass --username=wallet.user --password=wallet.pass
